56d99f609d9445b054770347bc153eb173894931
Implosions <ImplosionsTA@gmail.com>
2019-02-22 16:41:56 -0500

    Parse currency stack size

--- PoEMarketLookup/CurrencyParser.cs
+++ PoEMarketLookup/CurrencyParser.cs
@@ -1,22 +1,40 @@
-﻿namespace PoEMarketLookup
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace PoEMarketLookup
 {
     public class CurrencyParser
     {
         private static readonly string SECTION_SEPARATOR = new string('-', 8);
+        private static readonly Regex RE_STACK_SIZE = new Regex(@"Stack Size: (\d+)");
 
         private string[] itemSections;
+        private string itemBase;
+        private int stackSize;
 
         public CurrencyParser(string rawItemText)
         {
-            itemSections = rawItemText.Split(SECTION_SEPARATOR.ToCharArray());
+            itemSections = rawItemText.Split(SECTION_SEPARATOR.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
         }
 
-        public Currency Parse()
+        public Currency Parse() {
+            ParseInfoSection();
+            ParseCurrencyData();
+
+            return new Currency(itemBase, stackSize);
+        }
+
+        private void ParseInfoSection()
         {
             string[] itemInfo = itemSections[0].Split('\n');
-            string itemBase = itemInfo[1].Trim();
+            itemBase = itemInfo[1].Trim();
+        }
 
-            return new Currency(itemBase, 0);
+        private void ParseCurrencyData()
+        {
+            var match = RE_STACK_SIZE.Match(itemSections[1]);
+            var valGroup = match.Groups[1];
+            stackSize = int.Parse(valGroup.Value);
         }
     }
 }
--- PoEMarketLookupTests/CurrencyParserTest.cs
+++ PoEMarketLookupTests/CurrencyParserTest.cs
@@ -15,5 +15,14 @@ namespace PoEMarketLookupTests
 
             Assert.AreEqual("Exalted Orb", c.Base);
         }
+
+        [TestMethod]
+        public void CurrencyParserReturnsCurrencyObjWithCorrectStackSize()
+        {
+            CurrencyParser cp = new CurrencyParser(PoEItemData.Currency.EXALTED_ORB);
+            Currency c = cp.Parse();
+
+            Assert.AreEqual(9, c.StackSize);
+        }
     }
 }
