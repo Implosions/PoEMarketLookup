0abb3d36a88d79a10fa18b0abfdf190673fc8093
Implosions <ImplosionsTA@gmail.com>
2019-04-27 10:33:09 -0400

    Test armour normalizing quality from 0 quality

--- PoEMarketLookup/PoE/Items/Armor.cs
+++ PoEMarketLookup/PoE/Items/Armor.cs
@@ -5,5 +5,12 @@
         public int Armour { get; set; }
         public int EvasionRating { get; set; }
         public int EnergyShield { get; set; }
+
+        public int GetNormalizedArmourValue()
+        {
+            float ar = Armour * 1.2f;
+
+            return (int)ar;
+        }
     }
 }
---
+++ PoEMarketLookupTests/Items/ArmorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoEMarketLookup.PoE.Items;
+
+namespace PoEMarketLookupTests.Items
+{
+    [TestClass]
+    public class ArmorTest
+    {
+        [TestMethod]
+        public void GetNormalizedArmourValueReturnsQualityNormalizedArmourValueAt20QualityFor0QualityItems()
+        {
+            var armor = new Armor()
+            {
+                Armour = 100
+            };
+
+            Assert.AreEqual(120, armor.GetNormalizedArmourValue());
+        }
+    }
+}
