72d8670c86a34a4d7ae11c0aa5621894dcf0a96f
Implosions <ImplosionsTA@gmail.com>
2019-05-29 16:06:06 -0400

    Test pasting clipboard command

---
+++ PoEMarketLookup/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using PoEMarketLookup.ViewModels.Commands;
+using System.Windows;
+using System.Windows.Input;
+
+namespace PoEMarketLookup.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ICommand PasteFromClipboardCommand { get; }
+
+        public string ItemText { get; set; }
+
+        public MainWindowViewModel()
+        {
+            PasteFromClipboardCommand = new BasicCommand(SetItemTextFromClipboard);
+        }
+
+        private void SetItemTextFromClipboard(object param)
+        {
+            ItemText = GetClipboard();
+        }
+
+        protected virtual string GetClipboard()
+        {
+            return Clipboard.GetText();
+        }
+    }
+}
---
+++ PoEMarketLookupTests/ViewModels/MainWindowViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoEMarketLookup.ViewModels;
+
+namespace PoEMarketLookupTests.ViewModels
+{
+    [TestClass]
+    public class MainWindowViewModelTest
+    {
+        private class MockViewModel : MainWindowViewModel
+        {
+            protected override string GetClipboard()
+            {
+                return "foo bar";
+            }
+        }
+
+        [TestMethod]
+        public void PasteClipboardCommandSetsItemTextToClipboardValue()
+        {
+            var vm = new MockViewModel();
+            vm.PasteFromClipboardCommand.Execute(null);
+
+            Assert.AreEqual("foo bar", vm.ItemText);
+        }
+    }
+}
