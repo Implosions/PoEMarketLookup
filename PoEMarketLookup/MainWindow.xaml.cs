a5de831dcf148195d836804b0e4b8ab7bbc804b5
Implosions <ImplosionsTA@gmail.com>
2019-05-29 17:23:22 -0400

    Update prototype view

--- PoEMarketLookup/MainWindow.xaml.cs
+++ PoEMarketLookup/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PoEMarketLookup.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace PoEMarketLookup
     {
         public MainWindow()
         {
+            DataContext = new MainWindowViewModel();
             InitializeComponent();
         }
     }
