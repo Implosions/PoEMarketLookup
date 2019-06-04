e88da786f400c56166fc2a4aec09edc9e3f13f4c
Implosions <ImplosionsTA@gmail.com>
2019-06-04 14:46:30 -0400

    Test CreateViewModel sets the item base

--- PoEMarketLookup/ViewModels/ItemViewModel.cs
+++ PoEMarketLookup/ViewModels/ItemViewModel.cs
@@ -1,15 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using PoEMarketLookup.PoE.Items;
+using System.Collections.Generic;
 
 namespace PoEMarketLookup.ViewModels
 {
     public class ItemViewModel
     {
-        public string ItemBase { get; set; }
-        public string ItemName { get; set; }
+        public string ItemBase { get; private set; }
+        public string ItemName { get; private set; }
 
-        public ItemModContainer ItemEnchant { get; set; }
-        public IList<ItemStat> ItemStats { get; set; }
-        public IList<ItemModContainer> ItemImplicits { get; set; }
-        public IList<ItemModContainer> ItemExplicits { get; set; }
+        public ItemModContainer ItemEnchant { get; private set; }
+        public IList<ItemStat> ItemStats { get; private set; }
+        public IList<ItemModContainer> ItemImplicits { get; private set; }
+        public IList<ItemModContainer> ItemExplicits { get; private set; }
+
+        private ItemViewModel() { }
+
+        public static ItemViewModel CreateViewModel(PoEItem item)
+        {
+            return new ItemViewModel()
+            {
+                ItemBase = item.Base
+            };
+        }
     }
 }
---
+++ PoEMarketLookupTests/ViewModels/ItemViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PoEMarketLookup.PoE.Items;
+using PoEMarketLookup.ViewModels;
+
+namespace PoEMarketLookupTests.ViewModels
+{
+    [TestClass]
+    public class ItemViewModelTest
+    {
+        private class MockPoEItem : PoEItem
+        {
+        }
+
+        [TestMethod]
+        public void CreateViewModelReturnsViewModelWithItemsBase()
+        {
+            var item = new MockPoEItem()
+            {
+                Base = "Foo"
+            };
+            var vm = ItemViewModel.CreateViewModel(item);
+
+            Assert.AreEqual("Foo", vm.ItemBase);
+        }
+    }
+}
