4d0e69b8af78f0ae99d082872ce325a32a661ecf
Implosions <ImplosionsTA@gmail.com>
2019-06-06 16:11:07 -0400

    Test PoEJsonConverter has query property

---
+++ PoEMarketLookup/Web/PoEJsonConverter.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace PoEMarketLookup.Web
+{
+    public static class PoEJsonConverter
+    {
+        public static string SerializeSearchParameters()
+        {
+            var root = new JObject
+            {
+                new JProperty("query")
+            };
+            return root.ToString();
+        }
+    }
+}
---
+++ PoEMarketLookupTests/Web/PoEJsonConverterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using PoEMarketLookup.Web;
+
+namespace PoEMarketLookupTests.Web
+{
+    [TestClass]
+    public class PoEJsonConverterTest
+    {
+        [TestMethod]
+        public void SerializeSearchParametersHasQueryObject()
+        {
+            string json = PoEJsonConverter.SerializeSearchParameters();
+            var jo = JObject.Parse(json);
+
+            Assert.IsTrue(jo.ContainsKey("query"));
+        }
+    }
+}
