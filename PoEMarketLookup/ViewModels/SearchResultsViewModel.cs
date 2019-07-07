345f8171e59bd8ee05169bfb6b1333415f6b2c20
Implosions <ImplosionsTA@gmail.com>
2019-07-07 16:45:48 -0400

    CreateViewModel can parse id parameter

---
+++ PoEMarketLookup/ViewModels/SearchResultsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+namespace PoEMarketLookup.ViewModels
+{
+    public class SearchResultsViewModel
+    {
+        public string Id { get; set; }
+
+        public static SearchResultsViewModel CreateViewModel(string result)
+        {
+            var json = JToken.Parse(result);
+
+            return new SearchResultsViewModel()
+            {
+                Id = json["id"].ToString()
+            };
+        }
+    }
+}
---
+++ PoEMarketLookupTests/ViewModels/SearchResultViewModelTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using PoEMarketLookup.ViewModels;
+
+namespace PoEMarketLookupTests.ViewModels
+{
+    [TestClass]
+    public class SearchResultViewModelTest
+    {
+        private string _testJsonResult;
+
+        public SearchResultViewModelTest()
+        {
+            _testJsonResult = new JObject()
+            {
+                new JProperty("id", "foo")
+            }.ToString();
+        }
+
+        [TestMethod]
+        public void CreateViewModelSetsId()
+        {
+            var vm = SearchResultsViewModel.CreateViewModel(_testJsonResult);
+
+            Assert.AreEqual("foo", vm.Id);
+        }
+    }
+}
