14a57f4fe851e3ed2ca5698f135a5cb60599bec2
Implosions <ImplosionsTA@gmail.com>
2019-07-28 16:05:27 -0400

    Create Resources folder if it does not exist on startup

--- PoEMarketLookup/App.xaml.cs
+++ PoEMarketLookup/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,14 @@ namespace PoEMarketLookup
     /// </summary>
     public partial class App : Application
     {
+        private const string PATH_RESOURCES = @"Resources";
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (!Directory.Exists(PATH_RESOURCES))
+            {
+                Directory.CreateDirectory(PATH_RESOURCES);
+            }
+        }
     }
 }
