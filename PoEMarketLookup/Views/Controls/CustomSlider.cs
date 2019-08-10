ffe92f34ef858f8331cbb9f7fa31ddde5be23750
Implosions <ImplosionsTA@gmail.com>
2019-08-10 12:29:41 -0400

    Create double slider control

---
+++ PoEMarketLookup/Views/Controls/CustomSlider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace PoEMarketLookup.Views.Controls
+{
+    public class CustomSlider : Slider
+    {
+        public enum Direction
+        {
+            Down,
+            Up
+        }
+
+        public static readonly DependencyProperty ThumbDirectionProperty =
+            DependencyProperty.Register("ThumbDirection", typeof(Direction), typeof(CustomSlider));
+
+        public Direction ThumbDirection
+        {
+            get => (Direction)GetValue(ThumbDirectionProperty);
+            set => SetValue(ThumbDirectionProperty, value);
+        }
+
+        static CustomSlider()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomSlider),
+                new FrameworkPropertyMetadata(typeof(CustomSlider)));
+        }
+    }
+}
---
+++ PoEMarketLookup/Views/Controls/DoubleSlider.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows.Controls;
+
+namespace PoEMarketLookup.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for DoubleSlider.xaml
+    /// </summary>
+    public partial class DoubleSlider : UserControl
+    {
+        private double _minimum;
+        public double Minimum
+        {
+            get => _minimum;
+            set
+            {
+                _minimum = value;
+                MinValueSlider.Minimum = value;
+                MaxValueSlider.Minimum = value;
+            }
+        }
+
+        private double _maximum;
+        public double Maximum
+        {
+            get => _maximum;
+            set
+            {
+                _maximum = value;
+                MinValueSlider.Maximum = value;
+                MaxValueSlider.Maximum = value;
+            }
+        }
+        
+        public double MinValue
+        {
+            get => MinValueSlider.Value;
+            set
+            {
+                MinValueSlider.Value = value;
+            }
+        }
+        public double MaxValue
+        {
+            get => MaxValueSlider.Value;
+            set
+            {
+                MaxValueSlider.Value = value;
+            }
+        }
+
+        public DoubleSlider()
+        {
+            InitializeComponent();
+        }
+
+        private void MinValueSlider_ValueChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(MinValue > MaxValue)
+            {
+                MinValue = MaxValue;
+            }
+        }
+
+        private void MaxValueSlider_ValueChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(MaxValue < MinValue)
+            {
+                MaxValue = MinValue;
+            }
+        }
+    }
+}
